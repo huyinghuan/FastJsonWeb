540259506146b6e9b2999f4c9aae23289a8a745c
ec <ec.huyinghuan@gmail.com>
2013-05-22 14:01:56 +0800

    'second'

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var routerHandle = require("./request_handle");
 var fastjsonweb = require("fastjsonweb");
-fastjsonweb.setRouterHandle(routerHandle);
+fastjsonweb.setRouterHandle(routerHandle.handle);//设置映射关系
+//fastjsonweb.setStaticResourceDir("/static");//设置静态文件路径
 var app = fastjsonweb.createServer();
 app.listen(3000);
