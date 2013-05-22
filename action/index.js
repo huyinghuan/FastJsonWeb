fe4f8079a3f299187a54c0306605bedc165d79fe
ec@Ubuntu <xiacijian@163.com>
2013-05-23 00:08:05 +0800

    1.修改post数据无法注入bug
    1.增加动态方法调用测试
    2.增加post,get数据注入测试。
    3.增加router的功能选择

--- action/index.js
+++ action/index.js
@@ -1,6 +1,24 @@
 function Index(){
 	this.execute = function(request,response){
-
+		response.write('<h1 align="center">Index !!!!!!</h1>');
+		response.end();
+	},
+	this.dmi =  function(request,response){
+		response.write('dmi');
+		response.end();
+	},
+	this.ioc = function(request,response){
+		response.write('ioc  '+this.name);
+		response.end();
+	}
+	this.get = function(request,response){
+		response.write('get:  '+this.name);
+		response.end();
+	}
+	this.post = function(request,response){
+		response.write('post:  '+this.name);
+		response.end();
 	}
 }
+
 module.exports = Index;
--- request_handle.js
+++ request_handle.js
@@ -1,10 +1,6 @@
 /**
   请求映射
  */
-//exports.errorHandle= require('./action/nopath');//404错误处理
-
-//exports.staticResourceDir = "/public"; //设置静态资源文件夹
-
 exports.handle ={
   '/':require('./action/index'),
 };
