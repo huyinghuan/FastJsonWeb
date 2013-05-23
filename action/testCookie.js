893f4c951e1ccc423c5db2d7e630a1586d74c990
ec <ec.huyinghuan@gmail.com>
2013-05-23 17:00:20 +0800

    testCookie

--- action/testCookie.js
+++ action/testCookie.js
@@ -2,14 +2,10 @@ var Cookie = require("fastjsonweb").Cookie;
 function TestCookie(){
 	this.execute = function(request,response){
 		var cookie = new Cookie(request,response);
-		console.log("1: "+cookie.getValue("color"));
+	//	response.write("1: "+cookie.getValue("color"));
 		cookie.setValue("color","red");
 		console.log("2: "+cookie.getValue("color"));
 		cookie.flush();
-		response.on("close",function(){
-			console.log(123);
-		});
-		response.end();
 	}
 }
 
--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var fastjsonweb = require("fastjsonweb");
 fastjsonweb.setRouterHandle(routerHandle.handle);//设置映射关系
 //fastjsonweb.setStaticResourceDir("/static");//设置静态文件路径
 var app = fastjsonweb.createServer();
-app.listen(3000);
+app.listen(8888);
