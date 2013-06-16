78766194b0638bc874a996d3feb4073f7ea40857
ec@Home <ec.huyinghuan@gmail.com>
2013-06-16 17:08:52 +0800

    增加全局配制文件，配制静态资源目录和session有效时间

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ var routerHandle = require("./request_handle");
 var fastjsonweb = require("fastjsonweb");
 fastjsonweb.setRouterHandle(routerHandle.handle);//设置映射关系
 //fastjsonweb.setStaticResourceDir("/static");//设置静态文件路径
+fastjsonweb.globelConfigure({
+	static:"/static",
+	sessiontimeout:30
+});
 var app = fastjsonweb.createServer();
 app.listen(8888);
