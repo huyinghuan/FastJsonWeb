a7377d7f9eadd561cae3410b75efedd478637946
ec@Ubuntu <xiacijian@163.com>
2013-06-21 22:08:16 +0800

    增加网站ico图标识别
    1.增加网站ico图标识别
    2，修正写入json格式数据时增加了不该增加的json格式说明头信息
    3,修改静态文件读取方式。

--- action/testNewResponse.js
+++ action/testNewResponse.js
@@ -1,19 +1,19 @@
-var NewResponse = require("fastjsonweb").NewResponse;
+//var NewResponse = require("fastjsonweb").NewResponse;
 var fs = require('fs');
 function TestNewReponse(){
 	this.execute = function(request,response){
-		var newResponse = new NewResponse(response);
-		newResponse.write({a:123});
+	//	var newResponse = new NewResponse(response);
+		response.write({a:123,name:"avc"});
 	};
 	this.w = function(request,response){
 		var userFile= "F:\\a.doc";
-		var newResponse = new NewResponse(response);
+	//	var newResponse = new NewResponse(response);
 		fs.exists(userFile, function(exists) {
 			if (exists) {
 				var fileStream = fs.createReadStream(userFile);
-				newResponse.writeToFileByReadableStream(fileStream,{download:true,filename:"test.doc"});
+				response.writeToFileByReadableStream(fileStream,{download:true,filename:"test.doc"});
 			}else{
-				response.end("Thank you");
+				response.end();
 			}
 		});
 	}
