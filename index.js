066db2f0af0fa0b952ad619a41c32dac825c8ea9
ec@Ubuntu <xiacijian@163.com>
2013-07-03 00:29:34 +0800

    Add 404 page setting

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var fastjsonweb = require("fastjsonweb");
 fastjsonweb.setRouterHandle(routerHandle.handle);//设置映射关系
 //fastjsonweb.setStaticResourceDir("/static");//设置静态文件路径
 fastjsonweb.globelConfigure({
-	static:"/static",
-	sessiontimeout:30,
-	smartrouter:true
+	'static':"/static",
+	'sessiontimeout':30,
+	'smartrouter':true,
+  '404':require("./nopage")
 });
 var app = fastjsonweb.createServer();
 app.listen(8888);
