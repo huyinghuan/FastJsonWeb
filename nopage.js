5fc65bbca3013c6a9176b996f6bb558b104e7e6a
ec <xiacijian@163.com>
2013-09-09 14:18:36 +0800

    test orion

--- nopage.js
+++ nopage.js
@@ -1,6 +1,6 @@
 function Notpage(){
 	this.execute = function(request,response){
-    response.setStatus(404);
+    	response.setStatus(404);
 		var html = '<h1 align="center">404![ Defined by myself ]Path  "'+request.url+'" is Error!</h1>';
 		response.write(html,"html");
 	}
